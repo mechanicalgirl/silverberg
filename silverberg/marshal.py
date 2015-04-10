89a8864f9f7000b14de608d3bdbb21bd8d0e0364
Timothy Zhang <timium.zhang@gmail.com>
2015-04-10 16:13:25 +0800

    marshal None

--- silverberg/marshal.py
+++ silverberg/marshal.py
@@ -76,6 +76,8 @@ def marshal(term):
         # If the datetime is naive, then it is considered UTC time and stored. If it is
         # timezone-aware, then its corresponding UTC time is stored
         return str(int(calendar.timegm(term.utctimetuple()) * 1000 + term.microsecond / 1e3))
+    elif term is None:
+        return "null"
     else:
         return str(term)
 
